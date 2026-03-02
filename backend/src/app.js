a41e3f54d4b2775d97f290915db81ff2484632d3
Nitheshk42 <nitheshk42@gmail.com>
2026-03-02 18:53:23 -0500

    added the new site in CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://localhost:3000', 'https://localhost:3001', 'https://crobo-frontend.onrender.com'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://localhost:3000', 'https://localhost:3001', 'hhttps://dravina-62qg.onrender.com'],
   credentials: true,
 }));
 // For Stripe webhooks, we need raw body, so we set this route before the JSON parser
